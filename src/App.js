3c66645641f2b6d59ca64a205fc538d5a18cbfca
Lucas de Jesus <42447675+lucasdjesus@users.noreply.github.com>
2021-12-08 18:15:59 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ListaDeNotas } from './components/ListaDeNotas';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (              //Para o return, é necessário somente um componente.É necessário incluir tudo dentro de uma section
+  <section>
+   <form>
+    <input type="text" placeholder="Titulo"/>
+    <textarea placeholder="Escreva sua nota..."/>
+    <button>Cria Nota</button>
+   </form>
+   <ListaDeNotas/>
+   </section>
   );
 }
-
+//react -> lib
+//React -> ecossistema
 export default App;
